03846a1649702195f885aa58be0ac0239d778127
Dheemanth Narasimha <dheemanth.simha01@gmail.com>
2021-06-09 22:06:49 +0530

    Serialize and deserialize users via passport

--- app.js
+++ app.js
@@ -39,6 +39,14 @@ app.use(session({
 app.use(passport.initialize())
 app.use(passport.session())
 
+passport.serializeUser(function (user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function (user, done) {
+  done(null, user);
+});
+
 let price;
 
 
